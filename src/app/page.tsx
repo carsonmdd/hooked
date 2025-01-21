ebe592e96a28a63d30c41d02353d044bf7f44768
Carson Davis <cmd52465@gmail.com>
2025-01-20 23:51:09 -0800

    implement auth; add temp sidebar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
 	return (
-		<>
-			<h1>Hello World!</h1>
-		</>
+		<div className='flex items-center justify-center w-full h-full'>
+			<h1 className='text-5xl font-bold'>Hello World!</h1>
+		</div>
 	);
 }
